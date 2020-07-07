c7488d014a0e5bb32983c26923e52da4e4c90130
Hamoon Sheykh Hasani <hamoonshahsavar@gmail.com>
2020-07-08 02:39:22 +0430

    upload

---
+++ Hospital/obj/Release/Package/PackageTmp/Scripts/Site.js
@@ -0,0 +1,108 @@
+﻿$(document).ready(function () {
+    $(window).keydown(function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
+  
+    $('#mobileNO').keyup(function () {
+        var str = $('#mobileNO').val();
+        const regex = RegExp('09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}');
+        if (regex.test(str)) {
+            $('#btn').prop('disabled', false);
+            document.getElementById("error").innerHTML = "";
+        }
+        else {
+            document.getElementById("error").innerHTML = "Enter a valid phone number"
+        }
+    });
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var opacity;
+    var current = 1;
+    var steps = $("fieldset").length;
+
+    setProgressBar(current);
+
+    $(".next").click(function () {
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        //Add Class Active
+        $("#progressbar li")
+            .eq($("fieldset").index(next_fs))
+            .addClass("active");
+
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate(
+            { opacity: 0 },
+            {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        display: "none",
+                        position: "relative",
+                    });
+                    next_fs.css({ opacity: opacity });
+                },
+                duration: 500,
+            }
+        );
+        setProgressBar(++current);
+    });
+
+    $(".previous").click(function () {
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+
+        //Remove class active
+        $("#progressbar li")
+            .eq($("fieldset").index(current_fs))
+            .removeClass("active");
+
+        //show the previous fieldset
+        previous_fs.show();
+
+        //hide the current fieldset with style
+        current_fs.animate(
+            { opacity: 0 },
+            {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        display: "none",
+                        position: "relative",
+                    });
+                    previous_fs.css({ opacity: opacity });
+                },
+                duration: 500,
+            }
+        );
+        setProgressBar(--current);
+    });
+
+    function setProgressBar(curStep) {
+        var percent = parseFloat(100 / steps) * curStep;
+        percent = percent.toFixed();
+        $(".progress-bar").css("width", percent + "%");
+    }
+
+    $(".submit").click(function () {
+        return false;
+    });
+});
+function preload() {
+    $("#btn").addClass("next");
+}
+
+//document.getElementById("btn").addEventListener('click', function (e) {
+//    // some code;
+
+//    e.preventDefault();
+//}, false);
